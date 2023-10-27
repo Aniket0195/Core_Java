c87759524b677dd3c3f266cf4c7cac39936a7816
Aniket Gaikwad <144793942+Aniket0195@users.noreply.github.com>
2023-10-27 04:27:15 -0700

    Add files via upload

---
+++ JAVA/Day_2/AddTwoNo/addNo.java
@@ -0,0 +1,8 @@
+public class addNo {
+    public static void main(String[] args) {
+        int a=25,b=7;
+       System.out.println("Addition is :"+addi_.addition_(a,b));
+    }
+
+    
+}
---
+++ JAVA/Day_2/AddTwoNo/addi_.java
@@ -0,0 +1,8 @@
+public class addi_ {
+    public static int addition_(int c, int d) 
+    {
+        int S = c+d;
+       return S;
+    }
+    
+}
